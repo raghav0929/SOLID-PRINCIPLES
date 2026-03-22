6e5f43f925e2b891a2f802c6873f3c0d2d6403fd
raghav <raghavspb12@gmail.com>
2026-03-22 19:22:15 +0530

    Initial commit

---
+++ src/test/java/com/raghav/SOLID_PRINCIPLES/SolidPrinciplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.SOLID_PRINCIPLES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidPrinciplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
